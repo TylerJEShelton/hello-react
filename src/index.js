a170bb32164865aa5325e9225c590070d0aa567a
Tyler <tylershelton99@gmail.com>
2021-11-14 16:03:35 -0500

    Add input field, reset button, state and conditional functionality

--- src/index.js
+++ src/index.js
@@ -4,21 +4,29 @@ import './index.css';
 
 const Button = (props) => {
   // your code here
+  //const handleClick = () => console.log("reset");
+
+  return(
+    <button onClick={props.onClick}>{props.buttonText}</button>
+  );
 };
 
 const Application = () => {
 
   // your code here
+  const [name, setName] = useState("");
 
-  const reset = () => {
-    console.log("reset");
-    // your code here
-  };
+  const reset = () => setName("");
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Type your name"
+      />
+      <Button onClick={reset} buttonText="Reset" />
+      {name && <h1>Hello {name}</h1>}
     </main>
   );
 };
